30533fd1425f08f18c0071fdca0b9d5547e7799c
Curs279 <136276439+Curs279@users.noreply.github.com>
2023-11-13 02:59:55 +0000

    Se creó el generador

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  setInterval(function() {
+    document.querySelector(".valor").innerHTML = cambioDeValores();
+    document.querySelector("span").innerHTML = cambioDeSignos();
+  }, 10000);
+};
+
+document.getElementById("boton").addEventListener("click", botonCambiador);
+
+function botonCambiador() {
+  document.querySelector(".valor").innerHTML = cambioDeValores();
+  document.querySelector(
+    ".signo-arriba",
+    "signo-abajo"
+  ).innerHTML = cambioDeSignos();
+}
+
+let cambioDeValores = () => {
+  let valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let todosValores = Math.floor(Math.random() * valores.length);
+  return valores[todosValores];
+};
+
+let cambioDeSignos = () => {
+  let variosSignos = ["♣", "♦", "♥", "♠"];
+  let todosLosSignos = Math.floor(Math.random() * variosSignos.length);
+  return variosSignos[todosLosSignos];
 };
